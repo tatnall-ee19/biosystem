3ad6ef7cc3b8f958887251455bf6ef37c874bcd8
Mike SeaSand <msanders@tatnall.org>
2019-02-04 14:49:30 -0500

    adding stuff mkay

--- Grass_Trees/Grass_Trees.ino
+++ Grass_Trees/Grass_Trees.ino
@@ -3,39 +3,36 @@
 //Random Waving
 //Sensor Waving
 int Trees=2; // static , not essential to chain
-int GWaving=3; // not essential to chain
+int GWaving=3; // motion
 int GState1=4;
 int Deer=5;
 int GState2=6;
 int Wind=7; // not essential to chain
-int WindAudio=8; // audio
+int WindSound=8; // audio
 int River=9;
-int Sobir=10; 
-int Smani=11;
+int Sobir=10; //Song Birds
+int Smani=11; //Small Animals
 int Deeer; // variable of Deer
 void setup() {
   // put your setup code here , to run once:
+  digitalWrite(Trees,HIGH);
+  digitalWrite(Wind,HIGH);
+  digitalWrite(WindSound,HIGH);
+  digitalWrite(GWaving,HIGH);
+  digitalWrite(GState1,HIGH);
+  pinMode(Trees,INPUT);
+  pinMode(GWaving,INPUT);
+  pinMode(GState1,INPUT);
   pinMode(Deer,INPUT);
+  pinMode(GState2,INPUT);
+  pinMode(Wind,INPUT);
+  pinMode(WindSound,INPUT);
   pinMode(Sobir,OUTPUT);
   pinMode(River,OUTPUT);
   pinMode(Smani,OUTPUT);
-  pinMode(Smani,LOW);
-  pinMode(GState2,LOW);
-  pinMode(River,LOW);
-  pinMode(Sobir,LOW);
-  pinMode(GWaving,HIGH);
-  pinMode(GState1,HIGH);
-  pinMode(Trees,HIGH);
-  pinMode(Wind,HIGH);
-  pinMode(WindAudio,HIGH);
 }
 void loop() {
   // put your main code here , to run repeatedly:
-  digitalWrite(Trees,HIGH);
-  digitalWrite(Wind,HIGH);
-  digitalWrite(WindAudio,HIGH);
-  digitalWrite(GWaving,HIGH);
-  digitalWrite(GState1,HIGH);
   Deeer=digitalRead(Deer);
   delay(10000);
   digitalWrite(Deeer,HIGH);
