92eff5672bfab9ca373d0cf870f0ecaa1f9e2aee
Will <wsheehy@tatnall.org>
2019-02-08 10:59:40 -0500

    fixed everything

--- Beavers_Dam_SM._Animals/Beavers_Dam_SM._Animals/Beavers_dam_SM._Animals.ino
+++ Beavers_Dam_SM._Animals/Beavers_Dam_SM._Animals/Beavers_dam_SM._Animals.ino
@@ -1,31 +1,28 @@
-///////////////////////////////////////////////////////////////////
 //intial: few small animals, no bears
 //T1: add bears, dam, Send river signal
 //T2: more small animals, Send songbird signal
 //T3: more SA, send SA audio, send bears, Send birds of Prey
 //random SA audio after T3 only
-///////////////////////////////////////////////////////////////////
+
 int input_pin1 = 2;
 int input_pin2 = 3;
 int input_pin3 = 4;
 int reset_pin = 5;
-int final_output; //not sure why I need this, so i won't give #
 int initial_beavers = 6;
 int create_SA = 7;
 int dam_genesis = 8;
 int first_river_output = 9;
-int SA_primary_addition = 10; //on my arduino this pin doesn't work
+int SA_primary_addition = 10; 
 int songbird_output = 11;
 int SA_secondary_addition = 12;
 int Bears_signal = 13;
-int BOP_signal = 0; //what
-int SA_audio = 1; //what
+int BOP_signal = A0;
+int SA_audio = A1;
 
 void setup() {
 pinMode(input_pin1,INPUT);
 pinMode(input_pin2,INPUT);
 pinMode(input_pin3,INPUT);
-pinMode(final_output,OUTPUT);
 pinMode(initial_beavers,OUTPUT);
 pinMode(create_SA,OUTPUT);
 pinMode(SA_primary_addition,OUTPUT);
@@ -45,8 +42,7 @@ void loop() {
   initial_add();
   secondary_add();
   tertiary_add();
-  random_audio();
-  reset();
+  //random_audio();
 }
 
 void creation_state(){
@@ -71,9 +67,14 @@ void secondary_add(){
 void tertiary_add(){
   if(digitalRead(input_pin3) == HIGH){
     digitalWrite(SA_secondary_addition,HIGH);
-    digitalWrite(SA_audio,HIGH);
     digitalWrite(Bears_signal,HIGH);
     digitalWrite(BOP_signal,HIGH);
+    while(digitalRead(input_pin3 == HIGH)){
+      delay(10000);
+      digitalWrite(SA_audio,HIGH);
+      delay(1000);
+      digitalWrite(SA_audio,LOW);
+    }
   }
 }
 
