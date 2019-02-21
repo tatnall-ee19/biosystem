9a6a8fd329f95774c739f5b594997443f27b1d3a
Arjun Parmeswar <aparmeswar@protonmail.com>
2019-02-21 11:10:58 -0500

    Added a bunch of stuff. Main thing was getting the LEDs to work

--- River/River.ino
+++ River/River.ino
@@ -1,19 +1,19 @@
 /* River.ino - Contains the code for the river in the biosystem */
-#define	NUM_LEDS 2
+#define LED_PIN 5
+#define	NUM_LEDS 256
+#define LED_TYPE NEOPIXEL
+#define BRIGHTNESS 40
+#define COLOR_ORDER RGB 
 
 #include <FastLED.h>
 
 /* Global Variables */
-static const int _APINS[3] = {A0, A1, A2};
-static const int _RESET_PIN = 0;
-static const int _ROWS = 3;
-static const size_t _PINS_SZ = sizeof(_APINS) / sizeof(_APINS[0]);
-static int _velocities[_ROWS] = {12, -4, -23};
-static int _colors[3] = {1, 2, 3};
+static const int gRESET_PIN = 0;
+CRGB gLEDS[NUM_LEDS];
 
 /* Function definitons */
 static int Find_River_Color(int, int, int);
-static void Change_River_Color(int, int, int);
+static void Change_River_Color(int, int);
 static void Move_LED_Row(int, int);
 static void Flow(int);
 static void Stablize(void);
@@ -29,48 +29,26 @@ static void Reset(void);
 void setup() 
 {
     Serial.begin(9600);
-    for (size_t i = 0; i < _PINS_SZ - 1; i++) {
-	pinMode(_APINS[i], INPUT);
-    }
+    delay(300);
+    FastLED.addLeds<LED_TYPE, LED_PIN>(gLEDS, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
-    Change_River_Color(_colors, _APINS);
-    delay(50);
+    for(size_t i = 0; i <= NUM_LEDS; i++) {
+	gLEDS[i] = 0x0033cc; // Ultramarine
+	delay(10);
+    }
+    FastLED.show();
+
+    for(size_t i = 0; i <= NUM_LEDS; i++) {
+	gLEDS[i] = 0x0066cc;
+	delay(10);
+    }
+    FastLED.show();
+
+    delay(1);
 }
 
-static int Find_River_Color(int pin_one, int pin_two, int pin_three)
-{
-    int *color = malloc(3 * sizeof(int));
-    color[0] = digitalRead(pin_one); 
-    color[1] = digitalRead(pin_two);
-    color[2] = digitalRead(pin_three);
-    return color;
-}
 
-static void Change_River_Color(int *color, int *pins)
-{
-    size_t color_size = sizeof(color) / sizeof(color[0]);
-    size_t pins_size = sizeof(pins) / sizeof(pins[0]);
-
-    int *r, *g, *b = malloc(sizeof(int));
-
-    for (size_t i = 0; i <= color_size; i++) {
-	if (i = 0) 
-	    *r = color[i];
-	if (i = 1)
-	    *g = color[i];
-	if (i = 2)
-	    *b = color[i];
-    }
-
-    for (int j = 0; j <= 255; j+=4) {
-	analogWrite(*b, j);
-	delay(50);
-    }
-    for (int k = 255; k >= 0; k--) {
-	analogWrite(*b, k);
-	delay(50);
-    }
-}
